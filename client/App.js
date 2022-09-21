f7ba7537687987508794f72351d1d8563be3b9b0
macabrie <bliang95@gmail.com>
2022-09-20 21:10:28 -0400

    housekeeping in app.js, routes.js, and home.js to create homepage

--- client/App.js
+++ client/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import Navbar from './components/Navbar';
+import Routes from './Routes';
 
 const App = () => {
   return (
-    <div>
+    <div id="main">
       <Navbar />
       <Routes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
