a5447eb04701865a44fb95c430fb30378e724011
macabrie <bliang95@gmail.com>
2022-09-22 22:37:25 -0400

    added thunk and reducer to order reducer

--- client/store/order.js
+++ client/store/order.js
@@ -8,3 +8,25 @@ const _addProduct = (product) => ({
   type: ADD_PRODUCT,
   product,
 });
+
+//thunk creators
+export const addProduct = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: product } = await axios.post(`/api/order/${id}`, req.body);
+      dispatch(_addProduct(product));
+    } catch (error) {
+      next(err);
+    }
+  };
+};
+
+//reducer
+export default (state = {}, action) => {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return action.product;
+    default:
+      return state;
+  }
+};
