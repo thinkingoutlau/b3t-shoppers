500c590b3aaf823fed4389767d9b882249389360
macabrie <bliang95@gmail.com>
2022-09-22 21:59:55 -0400

    POST route to add products to order

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,6 +1,11 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
-const Order = db.define("order");
+const Order = db.define("order", {
+  status: {
+    type: Sequelize.STRING,
+    defaultValue: "unfulfilled",
+  },
+});
 
 module.exports = Order;
