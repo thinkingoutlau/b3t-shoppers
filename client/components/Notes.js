8441b6a6e2395eaabc6d5244de1f54e651ce7b3b
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-06 15:09:31 -0500

    changes

---
+++ client/components/Notes.js
@@ -0,0 +1,7 @@
+//Admin
+//- cannot edit or delete products
+//- alert if deleting user or product
+//- wishlist button does not work
+//- add to cart button does not work
+//- check if inventory decreases if user bought item
+//- check if price changes when updated on page
